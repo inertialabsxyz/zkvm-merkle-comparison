501d86a1d468709da17358b2ea4549b291d1e884
Andy Bell <andyjsbell@gmail.com>
2025-12-10 13:13:20 +0100

    feat: generic over levels

--- merkle-proof/src/lib.rs
+++ merkle-proof/src/lib.rs
@@ -1,5 +1,4 @@
 type Bytes32 = [u8; 32];
-const LEVELS: usize = 16;
 use bincode::error::{DecodeError, EncodeError};
 use bincode::{Decode, Encode};
 use sha2::{Digest, Sha256};
@@ -24,7 +23,7 @@ fn append32(a: &[u8; 32], b: &[u8; 32]) -> [u8; 64] {
     out
 }
 
-pub fn verify<H: MerkleHash>(proof: MerkleProof) -> bool {
+pub fn verify<H: MerkleHash, const N: usize>(proof: MerkleProof<N>) -> bool {
     let mut current = proof.leaf;
     let mut index = proof.index;
     for sibling in proof.siblings {
@@ -39,16 +38,17 @@ pub fn verify<H: MerkleHash>(proof: MerkleProof) -> bool {
     current == proof.root
 }
 
-#[derive(Encode, Decode, PartialEq, Debug)]
-pub struct MerkleProof {
-    root: Bytes32,
-    index: usize,
-    leaf: Bytes32,
-    siblings: [Bytes32; LEVELS],
+#[derive(Encode, Decode, PartialEq, Debug, Clone)]
+pub struct MerkleProof<const N: usize> {
+    pub root: Bytes32,
+    pub index: usize,
+    pub leaf: Bytes32,
+    pub siblings: [Bytes32; N],
 }
 
-pub fn create_proof<H: MerkleHash>(index: usize) -> MerkleProof {
-    let leaves: Vec<Bytes32> = (0..65536).map(|i: i32| H::hash(&i.to_le_bytes())).collect();
+pub fn create_proof<H: MerkleHash, const N: usize>(index: usize) -> MerkleProof<N> {
+    let size = 2u32.pow(N as u32);
+    let leaves: Vec<Bytes32> = (0..size).map(|i| H::hash(&i.to_le_bytes())).collect();
     // Create the bottom layer with the leaves
     let mut layers = vec![leaves];
     // We take the layer in pairs and append the hash to the layer above
@@ -64,7 +64,7 @@ pub fn create_proof<H: MerkleHash>(index: usize) -> MerkleProof {
     let leaf = layers[0][index];
     let mut siblings: Vec<[u8; 32]> = Vec::new();
     let mut idx = index;
-    for level in 0..16 {
+    for level in 0..N {
         let sibling_idx = if idx % 2 == 0 { idx + 1 } else { idx - 1 };
         siblings.push(layers[level][sibling_idx]);
         idx /= 2;
@@ -78,24 +78,24 @@ pub fn create_proof<H: MerkleHash>(index: usize) -> MerkleProof {
     }
 }
 
-pub fn save_proof(proof: &MerkleProof) -> Result<Vec<u8>, EncodeError> {
+pub fn save_proof<const N: usize>(proof: &MerkleProof<N>) -> Result<Vec<u8>, EncodeError> {
     bincode::encode_to_vec(proof, bincode::config::standard())
 }
 
-pub fn load_proof(bytes: &[u8]) -> Result<MerkleProof, DecodeError> {
+pub fn load_proof<const N: usize>(bytes: &[u8]) -> Result<MerkleProof<N>, DecodeError> {
     let (proof, _) = bincode::decode_from_slice(bytes, bincode::config::standard())?;
     Ok(proof)
 }
 
 #[test]
 fn test_verify() {
-    let index = 2345;
-    assert!(verify::<Sha256>(create_proof::<Sha256>(index)))
+    let index = 2;
+    assert!(verify::<Sha256, 2>(create_proof::<Sha256, 2>(index)))
 }
 
 #[test]
 fn save_and_load() {
-    let proof = create_proof::<Sha256>(42);
+    let proof = create_proof::<Sha256, 2>(2);
     let out = save_proof(&proof).expect("serialized proof");
     let proof_out = load_proof(&out).expect("deserialized proof");
     assert_eq!(proof, proof_out);
